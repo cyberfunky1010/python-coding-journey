c98a4a718b5bb658751724e1694a3f29dbb7730c
cyberfunky1010 <aquibbinali369@gmail.com>
2025-09-19 21:18:28 +0530

    upper_lower_digit.py program added

---
+++ conditionals/upper_lower_digit.py
@@ -0,0 +1,12 @@
+ch = input("enter a single character: ")
+
+if ch >= 'A' and ch <= 'Z':
+    print("you entered a UPPER case character")
+
+elif ch >= 'a' and ch <= 'z':
+    print("you entered a LOWER case character")
+
+elif ch >= '0' and ch <= '9':
+    print("you entered a digit")
+else:
+    ("you entered a special character")            
