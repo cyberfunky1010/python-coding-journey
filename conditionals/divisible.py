b248136e2716b6be01421381b775962ed26f2a49
cyberfunky1010 <aquibbinali369@gmail.com>
2025-09-19 20:28:03 +0530

    again commiting divisible.py file using terminal not GUI

--- conditionals/divisible.py
+++ conditionals/divisible.py
@@ -4,4 +4,4 @@ num2 = int(input("enter num2: "))
 if num1 % num2 == 0:
      print(num1,"is divisible by", num2)
 else:
-     print("not divisible")     
+     print(num1,"not divisible by",num2)     
