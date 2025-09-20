4af499c778975d820e8677b7f0ad32770f89f15b
cyberfunky1010 <aquibbinali369@gmail.com>
2025-09-20 15:55:05 +0530

    calculator program added

---
+++ conditionals/calculator.py
@@ -0,0 +1,21 @@
+# program that takes 2 inputs and calculate the result based on the operator user chooses.
+
+num1 = float(input("enter num 1: "))
+num2 = float(input("enter num 2: "))
+
+op = input("enter operator (+ - * / %)")
+
+if op == '+':
+    result = num1 + num2
+elif op == '-':
+    result = num1 - num2
+elif op == '*':
+    result = num1 * num2
+elif op == '/':
+    result = num1 / num2
+elif op == '%':
+    result = num1 % num2
+else:
+    print("you entered invalid operator")
+
+print(num1,op,num2," = ", result)                    
