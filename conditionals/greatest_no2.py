5956da3bf46315449f9f3b6027bf9dd6d7b2d45c
cyberfunky1010 <aquibbinali369@gmail.com>
2026-01-17 20:18:15 +0530

    two versions understand better

--- conditionals/greatest_no2.py
+++ conditionals/greatest_no2.py
@@ -1,14 +1,24 @@
+# find the greatest number.
+
 x = y = z = 0
 
 x = int(input("enter x: "))
 y = int(input("enter y: "))
 z = int(input("enter z: "))
 
-max = x
-if y > max:
-    max = y
+largest = x   # don't use max here. python will treat it as varible and won't treat it a function for the second part.
+if y > largest:
+    largest = y
 
-if z > max:
-    max = z    
+if z > largest:
+    largest = z    
 
-print("largest number is", max)    
+print("largest number is", largest)    
+
+# or you can simply do this.
+
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+c = int(input("enter c: "))
+
+print("largest num : ",max(a,b,c))
