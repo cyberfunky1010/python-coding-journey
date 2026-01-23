fb12b3c42e8536a4dad942f1c7fb095ee8503eda
cyberfunky1010 <aquibbinali369@gmail.com>
2026-01-23 23:06:59 +0530

    simple pattern program

---
+++ iterative-statements/pattern-programs/simple-star.py
@@ -0,0 +1,6 @@
+#simple star pattern
+
+for i in range(1,6):
+    for j in range(1,i):
+        print("*", end=" ")    #end="" at the end to cause printing at the same line.
+    print()    #to cause printing from next line.
