c1698c1c0f037d35a5f6e5774b2ea3e2e8ab229f
cyberfunky1010 <aquibbinali369@gmail.com>
2026-01-24 19:29:38 +0530

    program modified

--- string-manipulation/reverse-string.py
+++ string-manipulation/reverse-string.py
@@ -1,6 +1,17 @@
+# program to display reversed string. not acutally reversing a string.
+
 string1 = input("enter string to reverse: ")
 length = len(string1)
 print("word",string1,"in reverse order is ")
 
-for a in range(-1,(-length-1),-1):
+for a in range(-1,(-length-1),-1): #or (length-1,-1,-1) - this is more readable
     print(string1[a])
+    
+# same program with printing stirng in order and in reverse.
+
+string2 = input("enter string: ")
+length = len(string2)
+i = 0
+for a in range(length-1,-1,-1):
+    print(string2[i]," ",string2[a])
+    i+=1
