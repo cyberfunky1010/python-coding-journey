e0353ce9c54d5fd9e5484a2250aea25be8f806bf
cyberfunky1010 <aquibbinali369@gmail.com>
2026-01-26 20:37:09 +0530

    simpler version

--- iterative-statements/pattern-programs/pattern2.py
+++ iterative-statements/pattern-programs/pattern2.py
@@ -1,10 +1,10 @@
 rows = int(input("enter number of rows: "))
 
-for i in range(1, rows + 1):
-    for space in range(rows-i):
+for i in range(1, rows + 1):       #control rows 
+    for space in range(rows-i):    #shift spaces- centers the stars 
         print(end="  ")
 
-    for star in range(2*i-1):
+    for star in range(2*i-1):      #star loop   (2*i-1 prints star in odd number 1,3,5,6...form a triangle)
         print("* ", end="")
     
-    print()        
+    print()                        #moves cursor to the next line
