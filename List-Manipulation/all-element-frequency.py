6e5c5ec5674bc111857aae1adf57ed0d7258329a
cyberfunky1010 <aquibbinali369@gmail.com>
2026-04-13 00:53:01 +0530

    not gonna lie, this was tough program but did did it with help of book ):

---
+++ List-Manipulation/all-element-frequency.py
@@ -0,0 +1,30 @@
+# program to find frequency of all elements. also count duplicates and unique elements in the list. 
+
+lst = eval(input("enter list: "))
+length = len(lst)
+
+uniq = []
+dupl = []
+
+count = i = 0
+
+while i < length:
+    element = lst[i]
+    count = 1
+
+    if element not in uniq and element not in dupl:
+         i+=1
+         for j in range(i, length):
+             if element == lst[j]:
+                count += 1
+         else:
+             print("element: ", element, "frequency: ", count)
+             if count == 1:
+                 uniq.append(element)  
+             else:
+                 dupl.append(element)            
+    else:
+        i += 1
+print("original list: ",lst)        
+print("unique list: ",uniq)        
+print("duplicate list: ",dupl)        
