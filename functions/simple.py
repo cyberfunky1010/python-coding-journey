2fdd9ef25ea3dcc3f43720725b0f1478aae305e9
cyberfunky1010 <aquibbinali369@gmail.com>
2026-05-07 22:59:01 +0530

    common function programs

---
+++ functions/simple.py
@@ -0,0 +1,15 @@
+def greet():
+    print(f"whatever goes up always \ncomes down")
+    print(f" \t\t ~me ")
+
+def square(a):
+    return a*a
+
+def sumof3multiples(n):
+    s = n*1 + n*2 + n*3
+    print(s)
+  
+  
+sumof3multiples(2)
+print(square(2))   #here print is must cuz in funciton there is only return statement unlike in funciton sumof3multiple which has already print statement. 
+greet()
